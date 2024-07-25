8d93e711b61228063899b30b874d3b4fbf635d98
Grey <greywhat2002@gmail.com>
2024-07-25 10:40:31 +0200

    retracted previsous change

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw Error('Division by zero');
+    throw new Error('Division by zero');
   }
   return numerator / denominator;
 }
