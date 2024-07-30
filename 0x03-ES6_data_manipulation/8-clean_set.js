230cdd6bbbb1b9bde00ff8caa7aec4d83cd53364
Grey <greywhat2002@gmail.com>
2024-07-30 18:14:31 +0200

    concatenated strings in a set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,18 @@
+/**
+ * cleanSet - retruns a string of all set values that start with a specific string
+ * @param {Set} set
+ * @param {string} startString
+ * return {string}
+ */
+export default function cleanSet(set, startString) {
+  const tempArray = [];
+
+  if (startString === '') return '';
+
+  for (const string of set.values()) {
+    if (string.startsWith(startString)) {
+      tempArray.push(string.substring(startString.length));
+    }
+  }
+  return tempArray.join('-');
+}
