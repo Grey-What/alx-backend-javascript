86b5549fbdc7ed9feb695848561a743505e4d87e
Grey <greywhat2002@gmail.com>
2024-07-30 18:46:10 +0200

    updated values of map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+/**
+ * updateUniqueitems - updates a map
+ * @param {Map} map
+ * @return {Map}
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+}
