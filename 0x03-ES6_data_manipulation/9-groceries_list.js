452b27790eed30088697c24a64e83d2918b2a231
Grey <greywhat2002@gmail.com>
2024-07-30 18:39:23 +0200

    return map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+/**
+ * groceriesList - return Map
+ * return {Map}
+ */
+export default function groceriesList() {
+  const products = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
+  return new Map(products);
+}
