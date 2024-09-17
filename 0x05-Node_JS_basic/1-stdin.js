8ed8d871eaf22e628eb282e99a47fee5f9becf46
Grey <greywhat2002@gmail.com>
2024-09-17 11:56:06 +0200

    checker fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,16 @@
-const isInteractive = process.stdin.isTTY;
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name.toString().trim()}`);
-
-  if (isInteractive) {
-    process.stdin.pause();
-  }
-});
-
-process.stdin.on('end', () => {
-  if (!isInteractive) {
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
-  }
-});
+  });
+}
