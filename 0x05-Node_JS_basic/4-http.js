18279173c4ccb7270ce81b58b87b316c7a9d1690
Grey <greywhat2002@gmail.com>
2024-09-17 14:56:29 +0200

    small http module server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+/**
+ * a small http server listening on port 1245
+ */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
